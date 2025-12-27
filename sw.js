e5612b048938e138b94149d8fdaaa0ca3381dd86
x5vrjm6476-ux <x5vrjm6476@privaterelay.appleid.com>
2025-12-27 15:05:15 +0100

    sw.js erstellen

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "vibe-v2";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if(req.method !== "GET") return;
+  e.respondWith(
+    caches.match(req).then(cached => {
+      const fetcher = fetch(req).then(res=>{
+        const copy = res.clone();
+        if(new URL(req.url).origin === self.location.origin){
+          caches.open(CACHE).then(c=>c.put(req, copy));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached || fetcher;
+    })
+  );
+});
